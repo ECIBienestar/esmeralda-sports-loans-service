d05cc63c7cdd41833aaeee47af30ac6fc120e3b2
Sebastian Galvis Briceño <sebastian.galvis-b@mail.escuelaing.edu.co>
2025-05-03 01:32:54 -0500

    loan entity

---
+++ src/main/java/cvds/esmeralda/service/loans/entity/equipment/Equipment.java
@@ -0,0 +1,4 @@
+package cvds.esmeralda.service.loans.entity.equipment;
+
+public class Equipment {
+}
---
+++ src/main/java/cvds/esmeralda/service/loans/entity/loan/Loan.java
@@ -0,0 +1,27 @@
+package cvds.esmeralda.service.loans.entity.loan;
+
+import lombok.*;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.lang.annotation.Documented;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+//@Document(collection = "loans")
+public class Loan {
+    private String id;
+    private String userName;
+    private String userId;
+    private LocalDateTime dateAndTimeLoan;
+    private LocalDateTime dateAndTimeRepayment;
+    private String loanStatus;
+    private LocalDateTime dateAndTimeScheduleReturn;
+    private String duration;
+
+}
